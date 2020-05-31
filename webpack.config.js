f6334b0d3415bbe3a281af889f49d39b15531027
倪铖涛 <2922093951@qq.com>
2020-05-31 17:28:31 +0800

    '添加热更新和自动生成HTML插件'

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: path.join(__dirname, './main.js'),
   output: {
     path: path.join(__dirname, './dist'),
     filename: 'bundle.js'
-  }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin()
+  ]
 }
