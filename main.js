7b8385c6129c959f6eb7212430d2f7c70bf54003
倪铖涛 <2922093951@qq.com>
2020-06-01 07:43:19 +0800

    '安装vue-resource'

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './component/App.vue'
 import router from './router'
+import VueResource from 'vue-resource'
 
 import ElementUi from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css';
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUi)
+
+Vue.use(VueResource)
+
+// 创建根元素
+let root = document.createElement('div')
+root.setAttribute('id','app')
+document.body.appendChild(root)
 
 new Vue({
   el: '#app',
